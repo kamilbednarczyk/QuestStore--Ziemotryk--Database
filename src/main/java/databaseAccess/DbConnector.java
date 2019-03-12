6f76f115dff1543cc39ed197026adb9c4826b2bd
Ziemowit Błażków <yasha2yashaa@gmail.com>
2019-03-12 14:25:28 +0100

    Database opens and closes when connection to db needed

--- src/main/java/databaseAccess/DbConnector.java
+++ src/main/java/databaseAccess/DbConnector.java
@@ -13,13 +13,7 @@ public class DbConnector {
     }
 
     private DbConnector() {
-        try {
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/queststore_database", "queststore_user", "123"); // set user and password
-            statement = connection.createStatement();
-            System.out.println("Opened Database Succefully");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
     }
 
     public static DbConnector getInstance() {
@@ -32,20 +26,43 @@ public class DbConnector {
     }
 
     private void executeUpdate(String query) {
+        establishConnection();
         try {
             statement.executeUpdate(query);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        closeConnection();
     }
 
     private void setCurrentResultSetByQuery(String query) {
+        establishConnection();
         try {
             resultSet = statement.executeQuery(query);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        closeConnection();
     }
 
 
+    private void establishConnection() {
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/queststore_database", "queststore_user", "123"); // set user and password
+            statement = connection.createStatement();
+            System.out.println("Opened Database Succefully");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeConnection() {
+        try {
+            connection.close();
+            statement.close();
+            System.out.println("Closed Database Succefully");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
