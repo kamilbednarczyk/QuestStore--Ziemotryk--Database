5df1ed0b086fefd3426d7d020911dc9cdc5bd80e
PatrykMandrak <patrykmandrak123@gmail.com>
2019-03-12 13:27:03 +0100

    Set database name and user password, login to DB connector

--- src/main/java/databaseAccess/DbConnector.java
+++ src/main/java/databaseAccess/DbConnector.java
@@ -11,7 +11,7 @@ public class DbConnector {
 
     private DbConnector() {
         try {
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/databasename", "username", "password"); // set user and password
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/queststore_database", "queststore_user", "123"); // set user and password
             statement = connection.createStatement();
             System.out.println("Opened Database Succefully");
         } catch (SQLException e) {
