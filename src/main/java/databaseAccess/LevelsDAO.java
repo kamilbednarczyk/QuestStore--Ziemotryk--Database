71ddf9d55f48889ca7aebc307883e9a75aee5c93
Ziemowit Błażków <yasha2yashaa@gmail.com>
2019-03-14 08:20:11 +0100

    Implemented DAO in LevelsDAO

--- src/main/java/databaseAccess/LevelsDAO.java
+++ src/main/java/databaseAccess/LevelsDAO.java
@@ -2,32 +2,89 @@ package databaseAccess;
 
 import models.Level;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LevelsDAO implements IDAO<Level> {
 
+    DbConnector dbConnector = DbConnector.getInstance();
+
     @Override
     public void add(Level toAdd) {
-
+        dbConnector.executeUpdate(
+                "INSERT INTO levels"
+                        + "(level, coolcoins_needed, level_name, level_description)"
+                        + "VALUES(" + toAdd.getLevel() + ",\n"
+                        + "" + toAdd.getCoolcoinsNeeded() + ",\n"
+                        + "'" + toAdd.getLevelName() + ",\n"
+                        + "'" + toAdd.getLevelDescription() + ""
+                        + ");"
+        );
     }
 
     @Override
-    public Level get(int id) {
-        return null;
+    public Level get(int levelRow) {
+        ResultSet resultSet = dbConnector.getResultSetByQuery(
+                "SELECT * FROM levels WHERE level="+levelRow
+        );
+        Level level = null;
+
+        try {
+            resultSet.next();
+            level = new Level(
+                    resultSet.getInt("level"),
+                    resultSet.getInt("coolcoins_needed"),
+                    resultSet.getString("level_name"),
+                    resultSet.getString("level_description")
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return level;
     }
 
     @Override
     public List<Level> getAll() {
-        return null;
+        List<Level> levels = new ArrayList<>();
+        ResultSet resultSet = dbConnector.getResultSetByQuery(
+                "SELECT * FROM levels"
+        );
+
+        try {
+            while(resultSet.next()) {
+                levels.add(
+                        new Level(
+                                resultSet.getInt("level"),
+                                resultSet.getInt("coolcoins_needed"),
+                                resultSet.getString("level_name"),
+                                resultSet.getString("level_description")
+                        )
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return levels;
     }
 
     @Override
-    public void update(int id, Level toUpdate) {
-
+    public void update(int level, Level toUpdate) {
+        dbConnector.executeUpdate(
+                "UPDATE levels"
+                        +"SET level="+toUpdate.getLevel()+",\n"
+                        +"coolcoins_needed="+toUpdate.getCoolcoinsNeeded()+",\n"
+                        +"level_name='"+toUpdate.getLevelName()+"',\n"
+                        +"level_description='"+toUpdate.getLevelDescription()
+        );
     }
 
     @Override
-    public void delete(int id) {
-
+    public void delete(int level) {
+        dbConnector.executeUpdate(
+                "DELETE FROM levels WHERE level="+level
+        );
     }
 }
