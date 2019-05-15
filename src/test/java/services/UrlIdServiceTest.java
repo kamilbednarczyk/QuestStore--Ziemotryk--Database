967e6cbaa885bd967e8ff69d6ff424d00147260b
Kamil Bednarczyk <kamil2.bednarczyk@gmail.com>
2019-05-15 13:36:49 +0200

    Create UrlIdService test class

---
+++ src/test/java/services/UrlIdServiceTest.java
@@ -0,0 +1,12 @@
+package services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UrlIdServiceTest {
+
+    @Test
+    void getIdByUrl_When() {
+    }
+}
